e120d39dcd90a8e76b1998d32f7a46bab690dd7e
Shannon Barrett <Sbarrett00@live.com>
2016-10-07 14:50:40 -0500

    Updated to 4.0.0.64 and added checksum

--- SqlNexus/Tools/ChocolateyInstall.ps1
+++ SqlNexus/Tools/ChocolateyInstall.ps1
@@ -1,4 +1,7 @@
 $package = 'sqlnexus'
+$url = 'http://download-codeplex.sec.s-msft.com/Download/Release?ProjectName=sqlnexus&DownloadId=811886&FileTime=130445658852770000&Build=21031'
 $drop = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$checkSum = '1D5F7658E2ECD499E4BECE408980716C606818C3F5C2B419AFBAC7016A66BACA'
+$checkSumType = 'sha256'
 
-Install-ChocolateyZipPackage $package 'http://download-codeplex.sec.s-msft.com/Download/Release?ProjectName=sqlnexus&DownloadId=80378&FileTime=128956000434400000&Build=19471' $drop
+Install-ChocolateyZipPackage $package $url $drop -Checksum $checkSum -ChecksumType $checkSumType
